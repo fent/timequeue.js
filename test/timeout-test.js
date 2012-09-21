d27946ce44ff2e0ecc4392b3be1a3a25f57cd545
fent <933490+fent@users.noreply.github.com>
2012-09-21 08:42:39 -0700

    added `args` property on `err` object when a timeout happens
    this helps with debugging why the timeout might be happening
    by associating the error with all of the arguments of the task
    that timed out.

--- test/timeout-test.js
+++ test/timeout-test.js
@@ -17,21 +17,25 @@ describe('Create a queue with a timeout', function() {
   });
 
   describe('With tasks that lag', function() {
-    var q = new TimeQueue(function() {}, { timeout: 50 });
+    var q = new TimeQueue(function(a, b, callback) {}, { timeout: 50 });
 
     it('Should throw an error', function(done) {
-      q.push();
+      q.push(3, 4);
       q.on('error', function(err) {
         assert(err);
         assert.equal(err.message, 'Task timed out');
+        assert.equal(err.args[0], 3);
+        assert.equal(err.args[1], 4);
         done();
       });
     });
 
-    it('Should call the callback with the error', function() {
-      q.push(function(err) {
+    it('Should call the callback with the error', function(done) {
+      q.push('hello!', 'world', function(err) {
         assert(err);
         assert.equal(err.message, 'Task timed out');
+        assert.equal(err.args[0], 'hello!');
+        assert.equal(err.args[1], 'world');
         done();
       });
     });
